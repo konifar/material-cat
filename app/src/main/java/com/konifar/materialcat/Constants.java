570d9ab6edee5cb59da4c5c77ee1259e25932df8
konifar <yahpeycoy0403@gmail.com>
2017-08-11 23:21:23 +0900

    Changed flickr api key

--- app/src/main/java/com/konifar/materialcat/Constants.java
+++ app/src/main/java/com/konifar/materialcat/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static final String JSON_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String FLICKR_ENDPOINT = "https://api.flickr.com";
-    public static final String FLICKR_API_KEY = EnvConstants.FLICKR_API_KEY;
+    public static final String FLICKR_API_KEY = BuildConfig.FLICKR_API_KEY;
 
 }
